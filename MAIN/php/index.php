3398d6c85e5f1e1a497c0f5c6e4e283f5f0abfdb
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-04-09 09:50:00 -0300

    .

--- MAIN/php/index.php
+++ MAIN/php/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="../js/javascript.js"></script>
     <link rel="stylesheet" href="../css/style.css">
     <title>AutoChoice</title>
 </head>
@@ -11,19 +12,56 @@
         <img src="../images/wlogo.png" class="logo">
     </header>
     <main>
-        <section class="container">
-            <form action="$_POST" class="forml">
-                <input type="text" name="search" placeholder="O que deseja ver?">
-            </form>
+        <form action="" class="forml">
+            <input type="text" name="search" placeholder="O que deseja ver?" class="txt" id="forml">
+            <button type="submit" class="search"><img src="../images/lupa.png" class="lupa"></button>
+        </form> 
+        <section class="catalog" > 
+            <section class="line">
+                <div class="card">
 
-
-            
-
-        </section>
+                </div>
+                <div class="card">
+    
+                </div>
+                <div class="card">
+    
+                </div>
+                <div class="card">
+    
+                </div>
+                <div class="card">
+    
+                </div>
+                <div class="card">
+    
+                </div>    
+            </section>  
+        </section> 
+        
+        <div class="personal" id="personalPage">
+            <img src="../images/arrowright.png" class="arrowOpen" onclick="openPopUp()"> 
+        </div>
+        <div class="overlay" id="overlay">
+            <img src="../images/arrowleft.png" class="arrowClose" onclick="closePopUp()">
+            <div class="geral">
+                <div class="pageFunc">
+                    <img src="../images/profile1.png"><a href="profile.html" style="top: 60px">Perfil</a><br>
+                    <img src="../images/house.png" "><a href="index.html" style="top: 100px">Home</a><br>
+                    <img src="../images/heart.png" "><a href="index.html" style="top: 135px">Favoritos</a><br> 
+                </div>
+                <div class="config">
+                    <img src="../images/settings.png"><a href="settings.html" style="bottom: 115px">Configurações</a><br>
+                    <img src="../images/info.png" "><a href="info.html" style="bottom: 75px">Info</a><br> 
+                </div>
+            </div>
+        </div>
+        
+        
         
     </main>
-    <footer>
-
+    <footer id="footer">
+        <h5>Todos os diretos reservados - AutoChoice</h5>
     </footer>
 </body>
 </html>
