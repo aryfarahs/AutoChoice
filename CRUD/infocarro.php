b6bef419fc8d18e3e18a2f7c55f6ce026a867625
Ary Farah <124756642+aryfarahs@users.noreply.github.com>
2024-05-21 11:25:28 -0300

    .

--- CRUD/infocarro.php
+++ CRUD/infocarro.php
@@ -4,20 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title></title>
+    <style>
+        .tabelaInfo {
+            margin: auto;
+            margin-top: 100px;
+            border-collapse: collapse;
+        }
+
+        .tabelaInfo td, tr {
+            text-align: left;
+            padding: 5px;
+            border: 1px solid black;
+        }
+        
+        .altern {
+            background-color: lightgray;
+        }
+
+        .superContainer {
+            border: 1px solid red;
+            display: flex;
+        }
+
+        .editarCarro {
+            border: 1px solid red;
+            width: 40vw;
+        }
+
+        .butaozao {
+            padding: 5px;
+            margin-top: 30px;
+        }
+
+        .imagemzinha {
+            margin: auto;
+        }
+
+
+    </style>
+    <title>Atributos do Carro</title>
 </head>
 <body>
     <?php 
         
         require "header.php";
-        
-
-
 
         $oi = $_GET['id_carro'];
-
-
-
 
         $resultado = $banco->query(" select * FROM carro WHERE id_carro = $oi;");
 
@@ -27,31 +59,105 @@
         for($i = 1; $i <= $qtd; $i++){
             $objAtual = $resultado->fetch_object();
     ?>
-    <table>
-    <td>
-        
-                <tr><?php echo "Categoria: $objAtual->categoria";?><tr><br>
-                <tr><?php echo "Marca: $objAtual->marca";?><tr><br>
-                <tr><?php echo "Modelo: $objAtual->modelo";?><tdr<br>
-                <tr><?php echo "Motor: $objAtual->motor";?><tr><br>
-
-
-
-    </table>
-
-
+    <div class="superContainer">
+        <div class="editarCarro">
+            <img class="imagemzinha" src="images/settings.png">
+            <button class="butaozao">FHIHFSDAKJHFKDAJ</button>
+        </div>
+        <table class="tabelaInfo">
+            <tr>
+                <td class="altern">Categoria</td>
+                <td><?php echo "$objAtual->categoria";?></td>
+                <td class="altern">Quantidade de Airbags</td>
+                <td><?php echo "$objAtual->qntAirbags";?></td>  
+            </tr>
+            <tr>
+                <td class="altern">Marca</td>
+                <td><?php echo "$objAtual->marca";?></td>
+                <td class="altern">Estepe</td>
+                <td><?php echo "$objAtual->estepe";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Modelo</td>
+                <td><?php echo "$objAtual->modelo";?></td>
+                <td class="altern">Nota no Teste de Segurança</td>
+                <td><?php echo "$objAtual->notaTesteSeguranca";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Motor</td>
+                <td><?php echo "$objAtual->motor";?></td>
+                <td class="altern">Compatibilidade Apple/Android</td>
+                <td><?php if ("$objAtual->appleAndroid" == 1){echo "Sim";} else {echo "Não";}?></td>
+            </tr>
+            <tr>
+                <td class="altern">Potência</td>
+                <td><?php echo "$objAtual->potencia";?></td>
+                <td class="altern">Transmissão</td>
+                <td><?php echo "$objAtual->transmissao";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Quantidade de Lugares</td>
+                <td><?php echo "$objAtual->qntLugares";?></td>
+                <td class="altern">Porta-Malas</td>
+                <td><?php echo "$objAtual->portaMalas";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Ano</td>
+                <td><?php echo "$objAtual->ano";?></td>
+                <td class="altern">Altura</td>
+                <td><?php echo "$objAtual->altura";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Faixa de Preço</td>
+                <td><?php echo "$objAtual->faixaPreco";?></td>
+                <td class="altern">Largura</td>
+                <td><?php echo "$objAtual->largura";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Consumo na Estrada</td>
+                <td><?php echo "$objAtual->consumoEstrada";?></td>
+                <td class="altern">Comprimento</td>
+                <td><?php echo "$objAtual->comprimento";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Consumo na Cidade</td>
+                <td><?php echo "$objAtual->consumoCidade";?></td>
+                <td class="altern">0 a 100 km/h</td>
+                <td><?php echo "$objAtual->zeroACem";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Propulsão</td>
+                <td><?php echo "$objAtual->propulsao";?></td>
+                <td class="altern">Tração</td>
+                <td><?php echo "$objAtual->tracao";?></td>
+            </tr>
+            <tr>
+                <td class="altern">Torque</td>
+                <td><?php echo "$objAtual->torque";?></td>
+                <td class="altern">Importado</td>
+                <td><?php if ("$objAtual->importado" == 1){echo "Sim";} else {echo "Não";}?></td>
+            </tr>
+                <td class="altern">Câmera de Ré</td>
+                <td><?php if ("$objAtual->cameraRe" == 1){echo "Sim";} else {echo "Não";}?></td>
+                <td class="altern">Sensor de Estacionamento</td>
+                <td><?php if ("$objAtual->sensorEstacionar" == 1){echo "Sim";} else {echo "Não";}?></td>
+            <tr>
+                <td class="altern">Teto Solar</td>
+                <td><?php if ("$objAtual->tetoSolar" == 1){echo "Sim";} else {echo "Não";}?></td>
+                <td class="altern">Chave Presencial</td>
+                <td><?php if ("$objAtual->chavePresencial" == 1){echo "Sim";} else {echo "Não";}?></td>
+            </tr>
+            <tr>
+                <td class="altern">Farol de Neblina</td>
+                <td><?php if ("$objAtual->farolNeblina" == 1){echo "Sim";} else {echo "Não";}?></td>
+            </tr>
+        </table>
+    </div>
 
     <?php   
         }
 
     ?>
-
-
-
-
-
-
-
 
 
 </body>
